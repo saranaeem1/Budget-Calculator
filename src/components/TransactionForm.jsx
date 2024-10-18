2e8d7bc57010c187ab618592bbdb0bcefe464280
Sara Naeem <saranaeem754@gmail.com>
2024-10-18 19:28:16 +0500

    Updated

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -25,7 +25,9 @@ const TransactionForm = () => {
           onClick={() => setType("Income")}
           className={`mr-2 ${
             type === "Income" ? "bg-[#D9E7E5]" : "bg-gray-300"
-          } text-[#1F2937] rounded-lg border ${
+          } text-[#1F2937] ${
+            type === "Income" ? "font-bold" : "font-normal"
+          } rounded-lg border ${
             type === "Income" ? "border-[#42887C]" : "border-gray-300"
           }`}
           style={{
@@ -40,7 +42,9 @@ const TransactionForm = () => {
           onClick={() => setType("Expense")}
           className={`${
             type === "Expense" ? "bg-[#FFEAEA]" : "bg-gray-300"
-          } text-[#1F2937] rounded-lg border ${
+          } text-[#1F2937] ${
+            type === "Expense" ? "font-bold" : "font-normal"
+          } rounded-lg  border ${
             type === "Expense" ? "border-[#D14D4D]" : "border-gray-300"
           }`}
           style={{
